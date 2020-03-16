6d85d37fb271f708697f5a62e6af362818b859a1
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-16 22:03:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,8 @@ function sayHiToGrandma(string)
   if string.toLowerCase {
     return "I can't hear you!"
   }
-  if string.
+  if (string==="I love you, Grandma.")
+  {
+    return "I love you, too."
+  }
 }
